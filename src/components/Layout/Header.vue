397c1cc518f0c56f979e92c63d2171ef703b0641
OfficeFIeld <umair.anwar.arain@gmail.com>
2023-06-02 12:53:56 +0500

    UI fix
    UI fix

---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="header">
+    <h1> Main Header</h1>
+    <div class="leftTabs">
+        <ul>
+        <router-view>
+            <li> <router-link to="/explore">Explore</router-link> </li>
+            <li><router-link to="/function">Function</router-link></li>
+        </router-view>
+            <li>Function Page</li>
+            <li>Analytic Page</li>
+            <li>Comparer Page</li>
+            
+        </ul>
+
+    </div>
+   </div>
+
+</template>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css';
+import './assets/style.scss'
+import './router'
+
+
+
+createApp(App).use(router).mount('#app')
+  
